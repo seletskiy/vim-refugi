a3c920ec0c9dc5ea9258b8275c87b231d1b2928d
Stanislav Seletskiy <s.seletskiy@office.ngs.ru>
2012-02-02 15:07:31 +0700

    Reuse already opened master connections.
    Takes into account already opened master connections,
    so you can have only one ssh connection: an sshfs connection.
    All other commands will be going through it.

--- bin/git-wrapper.sh
+++ bin/git-wrapper.sh
@@ -2,6 +2,8 @@
 
 # TODO: documentation
 
+tmp_dir=/tmp
+
 get_mount() {
 	local dir=$1
 	local old_ifs=$IFS
@@ -22,11 +24,45 @@ get_mount() {
 	return 0
 }
 
-reopen_ssh_tunnel() {
+open_ssh_tunnel() {
 	local userhost=$1
 	local socket=$2
 
-	ssh -fNM -S "$socket" "$userhost"
+	if [ -z "$socket" ]; then
+		ssh -fNM "$userhost"
+	else
+		ssh -S "$socket" -fNM "$userhost"
+	fi
+}
+
+reopen_ssh_tunnel() {
+	local mount_userhost=$1
+
+	ssh_socket_flag=''
+	err_msg=`ssh -O check $mount_userhost 2>&1`
+	if [[ $? -gt 0 ]]; then
+		if [ "`echo $err_msg | grep '^No ControlPath'`" = "" ]; then
+			open_ssh_tunnel "$mount_userhost"
+		else
+			ssh_socket=$tmp_dir/refugi_${mount_userhost}_22
+
+			ssh -O check -S "$ssh_socket" "$mount_userhost" 2>/dev/null
+			if [[ $? -gt 0 ]]; then
+				open_ssh_tunnel "$mount_userhost" "$ssh_socket"
+			fi
+
+			ssh_socket_flag="-S "$ssh_socket
+		fi
+	fi
+
+	echo "$ssh_socket_flag"
+}
+
+cleanup_ssh_tunnels() {
+	for socket in `find $tmp_dir -name 'refugi_*' 2>/dev/null`; do
+		echo -n $socket": "
+		ssh -O exit -S "$socket" "${socket##refugi_}"
+	done
 }
 
 local_git_dir=''
@@ -35,6 +71,10 @@ for arg in "$@"; do
 	case $arg in
 		--git-dir=*)
 			local_git_dir=`echo $arg | cut -b11-`
+			;;
+		--close-ssh-tunnels)
+			cleanup_ssh_tunnels
+			exit $?
 			;;
 		*)
 			cmd_line[${#cmd_line[*]}]="$arg"
@@ -61,18 +101,14 @@ else
 		remote_git_dir=$mount_root/$remote_git_dir
 	fi
 
-	ssh_socket=/tmp/refugi_`echo $mount_dsn | sed -re's/[^a-z0-9_.-]/_/g'`
-	if [[ ! -a "$ssh_socket" ]]; then
-		reopen_ssh_tunnel "$mount_userhost" "$ssh_socket"
-	fi
-
-	ssh -O check -S "$ssh_socket" $mount_userhost 2>/dev/null
-	if [[ $? -gt 0 ]]; then
-		reopen_ssh_tunnel "$mount_userhost" "$ssh_socket"
-	fi
-
 	local_working_dir=`pwd`
 	remote_working_dir=${local_working_dir##$mount_point}
 
-	ssh -S $ssh_socket $mount_userhost cd "$remote_working_dir" \; git --git-dir="$remote_git_dir" "${cmd_line[@]}"
+	# `ssh -fNM ...` for some reason hangs script execution
+	tmp_file=`mktemp`
+	reopen_ssh_tunnel "$mount_userhost" > $tmp_file
+	ssh_socket_flag=`cat $tmp_file`
+	rm $tmp_file
+
+	ssh $ssh_socket_flag $mount_userhost cd "$remote_working_dir" \; git --git-dir="$remote_git_dir" "${cmd_line[@]}"
 fi
--- plugin/refugi.vim
+++ plugin/refugi.vim
@@ -7,6 +7,6 @@ let s:refugi_bin_dir = resolve(s:refugi_plugin_dir . "/../bin/")
 let s:refugi_git_wrapper_bin = s:refugi_bin_dir . "/git-wrapper.sh"
 let s:refugi_git_actual_bin = 'git'
 
-let g:fugitive_git_executable = s:refugi_git_wrapper_bin
+au VimEnter * let g:fugitive_git_executable = s:refugi_git_wrapper_bin
 
 " TODO: cleanup SSH tunnels
